75be48188e287d13202b4ea8f3f9464ed2d10a6b
Khushwant khatri <khatrikhushwant1811@gmail.com>
2026-03-28 18:37:57 +0000

    Added test_311_sample model

--- models/assignment_work/staging/test_311_sample.sql
+++ models/assignment_work/staging/test_311_sample.sql
@@ -6,15 +6,3 @@ SELECT
     borough
 FROM {{ source('raw', 'source_dot_service_requests_history') }}
 LIMIT 10
-```
-
-### Step 3 - Save the file
-Click **Save**
-
-### Step 4 - Commit
-Commit with message: `Added test_311_sample model`
-
-### Step 5 - Run the model
-In the terminal at the bottom type:
-```
-dbt run --select test_311_sample
